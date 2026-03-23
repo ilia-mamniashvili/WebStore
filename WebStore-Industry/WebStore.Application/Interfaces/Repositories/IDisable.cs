20655e029d4a670b7abeac0176afdf288dd59f4c
iliamamniashvili02@gmail.com <iliamamniashvili02@gmail.com>
2026-03-23 18:53:07 +0400

    Add IDisable interface and EnumerableHelper utility
    Added the IDisable interface in the WebStore.Application.Interfaces.Repositories namespace, featuring an Activity property of type ActivityInfo.
    
    Introduced the EnumerableHelper static class in the WebStore.Infrastructure namespace, which includes a ForEach<T> extension method for IEnumerable<T>. The method applies an action to each element with null checks for safety.

---
+++ WebStore-Industry/WebStore.Application/Interfaces/Repositories/IDisable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Application.Interfaces.Repositories
+{
+    public interface IDisable
+    {
+        ActivityInfo Activity { get; set; }
+    }
+}
